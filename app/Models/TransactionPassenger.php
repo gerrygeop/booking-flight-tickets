5da1e158b3c029124dbc36bf53245ee33119a725
gerrygeop <gerryja28@gmail.com>
2025-04-19 14:59:17 +0800

    feat(model): add function seat()

--- app/Models/TransactionPassenger.php
+++ app/Models/TransactionPassenger.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class TransactionPassenger extends Model
@@ -20,5 +21,10 @@ class TransactionPassenger extends Model
     public function transaction()
     {
         return $this->belongsTo(Transaction::class);
+    }
+
+    public function seat(): BelongsTo
+    {
+        return $this->belongsTo(FlightSeat::class);
     }
 }
