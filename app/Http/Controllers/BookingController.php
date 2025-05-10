4abb71755d3567b36a7382d612a2c39e0fd22c8d
gerrygeop <gerryja28@gmail.com>
2025-05-10 15:11:15 +0800

    feat: update bookign controller & add show request

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\BookingShowRequest;
 use App\Http\Requests\StorePassengerDetailRequest;
 use App\Interfaces\FlightRepositoryInterface;
 use App\Interfaces\TransactionRepositoryInterface;
@@ -34,6 +35,12 @@ class BookingController extends Controller
 
     public function confirmSeat(Request $request, $flightNumber)
     {
+        $request->validate(
+            [
+                'seat' => 'required'
+            ]
+        );
+
         $this->transactionRepository->saveTransactionDataToSession($request->all());
         return redirect()->route('booking.passengerDetails', ['flightNumber' => $flightNumber]);
     }
@@ -57,11 +64,71 @@ class BookingController extends Controller
         $transaction = $this->transactionRepository->getTransactionDataFromSession();
         $flight = $this->flightRepository->getFlightByFlightNumber($flightNumber);
         $tier = $flight->classes->find($transaction['flight_class_id']);
+
+        // dd($transaction);
         return view('pages.booking.checkout', compact('transaction', 'flight', 'tier'));
+    }
+
+    public function payment(Request $request)
+    {
+        $this->transactionRepository->saveTransactionDataToSession($request->all());
+        $transaction = $this->transactionRepository->saveTransaction(($this->transactionRepository->getTransactionDataFromSession()));
+        // dd($transaction);
+
+        \Midtrans\Config::$serverKey = config('midtrans.serverKey');
+        \Midtrans\Config::$isProduction = config('midtrans.isProduction');
+        \Midtrans\Config::$isSanitized = config('midtrans.isSanitized');
+        \Midtrans\Config::$is3ds = config('midtrans.is3ds');
+
+        $params = [
+            'transaction_details' => [
+                'order_id' => $transaction->code,
+                'gross_amount' => $transaction->grandtotal,
+            ]
+        ];
+
+        /**
+         * Midtrans sudah berhasil berjalan dengan baik
+         * Gunakan ngrok untuk melihat hasil direct pembayaran
+         * // $paymentUrl = \Midtrans\Snap::createTransaction($params)->redirect_url;
+         * // return redirect($paymentUrl);
+         */
+
+        /**
+         * Untuk sementara tidak akan menggunakan midtrans
+         * tetapi langsung direct ke halaman success
+         */
+        return redirect()->route(
+            'booking.success',
+            [
+                'order_id' => $params['transaction_details']['order_id']
+            ]
+        );
+    }
+
+    public function success(Request $request)
+    {
+        $transaction = $this->transactionRepository->getTransactionByCode($request->order_id);
+
+        if (!$transaction) {
+            return redirect()->route('home');
+        }
+        return view('pages.booking.success', compact('transaction'));
     }
 
     public function checkBooking()
     {
         return view('pages.booking.check-booking');
+    }
+
+    public function show(BookingShowRequest $request)
+    {
+        $transaction = $this->transactionRepository->getTransactionByCodePhone($request->code, $request->phone);
+
+        if (!$transaction) {
+            return redirect()->route('home');
+        }
+
+        return view('pages.booking.show', compact('transaction'));
     }
 }
---
+++ app/Http/Requests/BookingShowRequest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class BookingShowRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+            'code' => 'required',
+            'phone' => 'required',
+        ];
+    }
+}
