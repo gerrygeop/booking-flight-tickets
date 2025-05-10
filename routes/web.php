64d3faec953d79ac8f043c09a84cd36105e7f889
gerrygeop <gerryja28@gmail.com>
2025-05-10 15:11:35 +0800

    feat(route): add routes

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,9 @@ Route::get('flight/booking/{flightNumber}/passenger-details', [BookingController
 Route::post('flight/booking/{flightNumber}/save-passenger-details', [BookingController::class, 'savePassengerDetails'])->name('booking.savePassengerDetails');
 Route::get('flight/booking/{flightNumber}/checkout', [BookingController::class, 'checkout'])->name('booking.checkout');
 
+Route::post('flight/booking/{flightNumber}/payment', [BookingController::class, 'payment'])->name('booking.payment');
+
+Route::get('/booking-success', [BookingController::class, 'success'])->name('booking.success');
+
 Route::get('check-booking', [BookingController::class, 'checkBooking'])->name('booking.check');
+Route::post('check-booking', [BookingController::class, 'show'])->name('booking.show');
